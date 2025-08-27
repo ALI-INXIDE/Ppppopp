152c275e8f88541a8f4a764a6daf9cccf7541715
𝐀ɭīī 𝐈𝐍𝅦𝐗īī𝐃𝐄 ‹³策 <alirazalashari457@gmail.com>
2025-08-28 01:42:39 +0500

    Update dl.js

--- plugins/dl.js
+++ plugins/dl.js
@@ -165,3 +165,50 @@ gmd({
         reply("An error occurred. Please try again.");
     }
 });
+cmd({ 
+    pattern: "sg", 
+    alias: ["ytdl3", "song"], 
+    react: "🎶", 
+    desc: "Download YouTube song", 
+    category: "main", 
+    use: '.song < Yt url or Name >', 
+    filename: __filename 
+}, async (Gifted, mek, m, { from, prefix, quoted, q, reply }) => { 
+    try { 
+        if (!q) return await reply("*𝐏ℓєαʂє 𝐏ɼ๏νιɖє 𝐀 𝐘ʈ 𝐔ɼℓ ๏ɼ 𝐒๏ƞ͛g 𝐍αмє.*");
+        
+        const yt = await ytsearch(q);
+        if (yt.results.length < 1) return reply("No results found!");
+        
+        let yts = yt.results[0];  
+        let apiUrl = `https://apis.davidcyriltech.my.id/youtube/mp3?url=${encodeURIComponent(yts.url)}`;
+        
+        let response = await fetch(apiUrl);
+        let data = await response.json();
+        
+        if (data.status !== 200 || !data.success || !data.result.downloadUrl) {
+            return reply("Failed to fetch the audio. Please try again later.");
+        }
+        
+        let ytmsg = `*PLAYING: ${yts.title}*`;
+        
+        
+        // Send thumbnail with caption
+        await Gifted.sendMessage(from, { 
+            image: { url: yts.thumbnail }, 
+            caption: ytmsg,  
+        }, { quoted: mek });
+
+        // Send the audio directly
+        await Gifted.sendMessage(from, { 
+            audio: { url: data.result.downloadUrl }, 
+            mimetype: "audio/mpeg", 
+            fileName: `${yts.title}.mp3`, 
+        }, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e);
+        reply("An error occurred. Please try again later.");
+    }
+});
+          
